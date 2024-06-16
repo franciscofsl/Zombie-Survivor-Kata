094db3c24a47fd76fa301487bcc89b299d6de043
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-16 11:38:05 +0200

    change hurt check

--- src/ZombieSurvivor.Core/Survivor.cs
+++ src/ZombieSurvivor.Core/Survivor.cs
@@ -24,7 +24,7 @@ public sealed class Survivor
 
     public void Hurt()
     {
-        if (Wounds == MaxWoundsToDie)
+        if (IsDie())
         {
             throw ZombieSurvivorException.CannotHurtADeadSurvivor();
         }
