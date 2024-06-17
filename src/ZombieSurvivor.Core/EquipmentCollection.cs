e48aefa15d00b04d256469421ed9c195d5943866
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-17 19:10:19 +0200

    encapsulate equipment in class

---
+++ src/ZombieSurvivor.Core/EquipmentCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace ZombieSurvivor.Core;
+
+public class EquipmentCollection : IEnumerable<Item>
+{
+    private readonly List<Item> _items;
+
+    internal EquipmentCollection(int maxItems)
+    {
+        _items = new List<Item>(maxItems);
+    }
+
+    public IEnumerator<Item> GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    internal void AddItem(Item item)
+    {
+        _items.Add(item);
+    }
+}
---
+++ src/ZombieSurvivor.Core/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace ZombieSurvivor.Core;
+
+public class Item
+{
+    private readonly Name _name;
+
+    private Item(Name name)
+    {
+        _name = name;
+    }
+
+    public static Item Create(Name name)
+    {
+        return new Item(name);
+    }
+}
