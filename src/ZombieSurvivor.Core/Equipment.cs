4f6fa35c1b9aa1e9f9d8d55dbe62000eacc25676
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-17 19:10:09 +0200

    Add equipment

---
+++ src/ZombieSurvivor.Core/Equipment.cs
@@ -0,0 +1,21 @@
+﻿namespace ZombieSurvivor.Core;
+
+public sealed class Equipment
+{
+    private const int MaxItemsInHand = 2;
+    private readonly EquipmentCollection _inHand;
+
+    private Equipment()
+    {
+        _inHand = new EquipmentCollection(MaxItemsInHand);
+    }
+
+    public static Equipment Default => new();
+
+    internal void AddItem(Item item)
+    {
+        _inHand.AddItem(item);
+    }
+
+    internal EquipmentCollection InHand() => _inHand;
+}
