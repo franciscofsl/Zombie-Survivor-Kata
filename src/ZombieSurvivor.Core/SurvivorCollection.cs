44603ae8a0d7ee1c3fee7f0a3248e8cb1e6d6c67
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-19 19:46:22 +0200

    add survivor collection

---
+++ src/ZombieSurvivor.Core/SurvivorCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace ZombieSurvivor.Core;
+
+public class SurvivorCollection : IEnumerable<Survivor>
+{
+    public static SurvivorCollection Empty => new();
+    
+    private readonly List<Survivor> _survivors = new();
+
+    private SurvivorCollection()
+    {
+    }
+
+    public IEnumerator<Survivor> GetEnumerator() => _survivors.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
