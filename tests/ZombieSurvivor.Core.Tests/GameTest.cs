143ec261a8fd2ff4c70fbf3d77087aab00a799ef
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-19 19:52:15 +0200

    add test to ensure add survivor in game

--- tests/ZombieSurvivor.Core.Tests/GameTest.cs
+++ tests/ZombieSurvivor.Core.Tests/GameTest.cs
@@ -11,4 +11,14 @@ public class GameTest
 
         game.NumberOfSurvivors().Should().Be(0);
     }
+
+    [Fact]
+    public void Should_Add_Survivor_To_Game()
+    {
+        var game = Game.Start();
+
+        game.AddSurvivor("Ace");
+
+        game.NumberOfSurvivors().Should().Be(1);
+    }
 }
