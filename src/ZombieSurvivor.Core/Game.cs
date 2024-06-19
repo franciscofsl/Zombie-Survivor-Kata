385857ddb993e30b418144f1dc15e12707002f5f
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-19 19:42:29 +0200

    Add game type

---
+++ src/ZombieSurvivor.Core/Game.cs
@@ -0,0 +1,19 @@
+﻿namespace ZombieSurvivor.Core;
+
+public class Game
+{
+    private Game()
+    {
+        
+    }
+
+    public static Game Start()
+    {
+        return new Game();
+    }
+
+    public int NumberOfSurvivors()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/ZombieSurvivor.Core.Tests/GameTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+
+namespace ZombieSurvivor.Core.Tests;
+
+public class GameTest
+{
+    [Fact]
+    public void Game_Should_Start_Without_Players()
+    {
+        var game = Game.Start();
+
+        game.NumberOfSurvivors().Should().Be(0);
+    }
+}
