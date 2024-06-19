e317b543ab4b16af79672bdf022e7efe121b7185
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-19 19:52:56 +0200

    change collection initialization

--- src/ZombieSurvivor.Core/SurvivorCollection.cs
+++ src/ZombieSurvivor.Core/SurvivorCollection.cs
@@ -5,11 +5,12 @@ namespace ZombieSurvivor.Core;
 public class SurvivorCollection : IEnumerable<Survivor>
 {
     public static SurvivorCollection Empty => new();
-    
-    private readonly List<Survivor> _survivors = new();
+
+    private readonly List<Survivor> _survivors;
 
     private SurvivorCollection()
     {
+        _survivors = new List<Survivor>();
     }
 
     public IEnumerator<Survivor> GetEnumerator() => _survivors.GetEnumerator();
