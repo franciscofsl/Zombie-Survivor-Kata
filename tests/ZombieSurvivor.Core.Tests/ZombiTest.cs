abc0dd1609a32fc928570405a41d6fe296eaa83c
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-20 20:23:05 +0200

    add zombi test to ensure zombi give one experience when dies

---
+++ tests/ZombieSurvivor.Core.Tests/ZombiTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+
+namespace ZombieSurvivor.Core.Tests;
+
+public class ZombiTest
+{
+    [Fact]
+    public void Zombi_Should_Give_1_Experience_When_Dies()
+    {
+        var zombi = Zombi.Create();
+
+        zombi.ExperienceAtDie().Should().Be(1);
+    }
+}
