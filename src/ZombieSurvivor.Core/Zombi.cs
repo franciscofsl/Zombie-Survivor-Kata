0a24c88e30c05d1af5f826d9f5ee1eb338aa1390
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-20 20:23:21 +0200

    add zombi logic to give one experience when dies

---
+++ src/ZombieSurvivor.Core/Zombi.cs
@@ -0,0 +1,12 @@
+﻿namespace ZombieSurvivor.Core;
+
+public class Zombi : IEnemy
+{
+    private Zombi()
+    {
+    }
+
+    public static Zombi Create() => new();
+
+    public Experience ExperienceAtDie() => Experience.Of(1);
+}
