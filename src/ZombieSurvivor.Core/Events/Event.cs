cdca20d500958c1cc112b6b8251c60ab9a53b723
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-23 12:29:18 +0200

    add event

---
+++ src/ZombieSurvivor.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace ZombieSurvivor.Core.Events;
+
+public abstract class Event
+{
+    protected Event(string message)
+    {
+        Message = message;
+        OcurredOn = DateTime.Now;
+    }
+
+    public string Message { get; }
+
+    public DateTime OcurredOn { get; }
+}
