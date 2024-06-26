89373322d180b41f21b9ef1c9d47c4a7ed1aa0f3
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-26 21:17:29 +0200

    add virtual method to skill to separate skills benefits

--- src/ZombieSurvivor.Core/Skills/Skill.cs
+++ src/ZombieSurvivor.Core/Skills/Skill.cs
@@ -1,5 +1,11 @@
 ﻿using ZombieSurvivor.Core.Common;
+using ZombieSurvivor.Core.Survivors;
 
 namespace ZombieSurvivor.Core.Skills;
 
-public record Skill(string Name, Level Level);
+public record Skill(string Name, Level Level)
+{
+    internal virtual void Apply(Survivor survivor)
+    {
+    }
+}
